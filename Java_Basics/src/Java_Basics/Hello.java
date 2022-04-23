6e5258e52cc3627a5676ee486898cc7f0dd1a7a7
Kimberly Bryant <bryantkim702@gmail.com>
2022-04-23 08:51:16 -0700

    editing for better readability

--- Java_Basics/src/Java_Basics/Hello.java
+++ Java_Basics/src/Java_Basics/Hello.java
@@ -2,8 +2,8 @@ package Java_Basics;
 
 public class Hello {
 
-	public static void main(String[] args) {
-	//To print something to the console/screen
-	System.out.println("Hello there, my name is Kim!");	
-	}
+    public static void main(String[] args) {
+        //To print something to the console/screen
+        System.out.println("Hello there, my name is Kim!");
+    }
 }
--- Java_Basics/src/Java_Basics/LearningArrays.java
+++ Java_Basics/src/Java_Basics/LearningArrays.java
@@ -2,25 +2,25 @@ package Java_Basics;
 
 public class LearningArrays {
 
-	public static void main(String[] args) {
-		int [] values = new int[100];//declaring variable of type int in array with 100 slots 
-		values[0] = 1000;
-		values[1] = 55;
-		values[99] = 93432;
-		
-		System.out.println(values[1]);
-		
-		String[] words = new String[] {"My", "Name", "is"};
-		System.out.println(words[2]);
-	
-	// OR
-	String[] words1 = new String[3];
-	words1[0] = "My";
-	words1[1] = "Name";
-	words1[2] = "is";
-	System.out.println(words1[0]);
-	
-	
-}
+    public static void main(String[] args) {
+        int[] values = new int[100];//declaring variable of type int in array with 100 slots
+        values[0] = 1000;
+        values[1] = 55;
+        values[99] = 93432;
+
+        System.out.println(values[1]);
+
+        String[] words = new String[]{"My", "Name", "is"};
+        System.out.println(words[2]);
+
+        // OR
+        String[] words1 = new String[3];
+        words1[0] = "My";
+        words1[1] = "Name";
+        words1[2] = "is";
+        System.out.println(words1[0]);
+
+
+    }
 
 }
