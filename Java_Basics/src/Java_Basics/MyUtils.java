a51a59d00590818e2574a13091e8d0bca76ac7a7
Kim <99856852+KBryant702@users.noreply.github.com>
2022-04-30 14:23:27 -0700

    method visibility

--- Java_Basics/src/Java_Basics/MyUtils.java
+++ Java_Basics/src/Java_Basics/MyUtils.java
@@ -1,16 +1,13 @@
 package Java_Basics;
 
-public class MyUtils {
+ public class MyUtils {
 	
-//	public static void printSomeJunk(String argument) {
-//		System.out.println("printSomeJunk Method" + argument);
-//	}
-//	public static void printSomeJunk(int argument) {
-//		System.out.println("Integer passed in:" + argument);
-//	}
 	public static String printSomeJunk(String argument) {
-		String result = argument;
-		return result;
+//		System.out.println("printSomeJunk Method" + argument);
+		return argument;
+	}
+	public static void printSomeJunk(int argument) {
+		System.out.println("Integer passed in:" + argument);
 	}
 	public static void sum2Numbers(int firstArg, int secondArg){
 		System.out.println(firstArg + secondArg);
---
+++ Java_Basics/src/someOtherPackage/ExampleClass.java
@@ -0,0 +1,11 @@
+package someOtherPackage;
+
+import Java_Basics.MyUtils;
+
+public class ExampleClass {
+
+	public static void doSomething() {
+		MyUtils.printSomeJunk("Print Some junk from the doSomething Method");
+	}
+
+}
