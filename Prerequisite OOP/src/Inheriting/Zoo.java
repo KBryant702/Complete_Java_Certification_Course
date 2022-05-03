cb09789402b4f0068902ec98b8d1751612b33397
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-02 20:14:42 -0700

    Polymorphism

--- Prerequisite OOP/src/Inheriting/Zoo.java
+++ Prerequisite OOP/src/Inheriting/Zoo.java
@@ -11,17 +11,20 @@ public class Zoo {
 		
 //		animal1.speak();
 //		animal1.eat();
-		chick1.speak();
-		sparrow1.speak();
-		sparrow1.move();
-		sparrow1.fly();
+//		chick1.speak();
+//		sparrow1.speak();
+//		sparrow1.move();
+//		sparrow1.fly();
 
 		
 		Fish fish1 = new Fish("Guppy", 1, 1, 'M');
-		
-		fish1.speak();
-		fish1.swim();
-		fish1.move();
+		 moveAnimals(fish1);
+		 moveAnimals(sparrow1);
+//		fish1.speak();
+//		fish1.swim();
+//		fish1.move();
 	}
-
+	public static void moveAnimals(Animal animal) {
+		animal.move();
+}
 }
