7c020d30349de16293ba08968aaf3e21d99b27e7
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-02 22:21:53 -0700

    abstract classes

--- Prerequisite OOP/src/Inheriting/Zoo.java
+++ Prerequisite OOP/src/Inheriting/Zoo.java
@@ -3,28 +3,8 @@ package Inheriting;
 public class Zoo {
 
 	public static void main(String[] args) {
-		
-		Chicken chick1 = new Chicken("Chicken", 1, 3, 'F' );
-//		Animal animal1 = new Animal("Snake", 16, 60, 'F');
-		Sparrow sparrow1 = new Sparrow("Sparrow", 3, 1, 'M');
-		
-		
-//		animal1.speak();
-//		animal1.eat();
-//		chick1.speak();
-//		sparrow1.speak();
-//		sparrow1.move();
-//		sparrow1.fly();
-
-		
-		Fish fish1 = new Fish("Guppy", 1, 1, 'M');
-		 moveAnimals(fish1);
-		 moveAnimals(sparrow1);
-//		fish1.speak();
-//		fish1.swim();
-//		fish1.move();
+	
+		Flyable flyingBird = new Sparrow("Sparrow", 1, 2, 'F');
+		Bird flyingBird2 = new Bird("Sparrow", 1, 2, 'F');
 	}
-	public static void moveAnimals(Animal animal) {
-		animal.move();
-}
 }
