dd5a97963099dc4e09c69c4b28c4ca04b31d5709
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-13 21:56:29 -0700

    More Strings

--- prereq-practice/src/practice/strings/StringsPractice.java
+++ prereq-practice/src/practice/strings/StringsPractice.java
@@ -3,13 +3,32 @@ package practice.strings;
 public class StringsPractice {
 
 	public static void main(String[] args) {
-		String a = "hello";
-		
-		String str = "ABCDEFG";
-		String extractedString = str.substring(1);
-		
-		System.out.println(extractedString);
+		//problem 1
+//		String a = "hello";
+//		
+//		String str = "ABCDEFG";
+//		String extractedString = str.substring(1);
+//		
+//		System.out.println(extractedString);
 
-	}
+//	}
+		//problem 2
+//	String a = "hello";
+//	String b = "there";
+//	
+//	if(!a.equals("hello")) {
+//	}
+//	if (b.equalsIgnoreCase("There")){
+//		System.out.println("Printed there");
+//	}
+
+	String a = "hello";
+	String b = "there";
+	
+	a.charAt(3);
+	System.out.println(a.charAt(4));
+}
 
 }
+
+
