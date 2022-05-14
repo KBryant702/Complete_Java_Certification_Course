947f4ae6d52b3bbeb59c644d332bb9582f109243
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-13 22:23:39 -0700

    Index Of

--- prereq-practice/src/practice/strings/StringsPractice.java
+++ prereq-practice/src/practice/strings/StringsPractice.java
@@ -3,7 +3,7 @@ package practice.strings;
 public class StringsPractice {
 
 	public static void main(String[] args) {
-		//problem 1
+		// problem 1
 //		String a = "hello";
 //		
 //		String str = "ABCDEFG";
@@ -12,7 +12,7 @@ public class StringsPractice {
 //		System.out.println(extractedString);
 
 //	}
-		//problem 2
+		// problem 2
 //	String a = "hello";
 //	String b = "there";
 //	
@@ -21,14 +21,38 @@ public class StringsPractice {
 //	if (b.equalsIgnoreCase("There")){
 //		System.out.println("Printed there");
 //	}
+// problem 3 charAt index of a word
+//		String a = "hello";
+//		String b = "there";
+//
+//		a.charAt(3);
+//		System.out.println(a.charAt(4));
+		//problem 4
+		//indexOf method indexOf(String s)  or can have it start at an index
+//		String str = "Hello there yogi";
+//		
+//		int a = str.indexOf("there");
+//		System.out.println(a);
+//		//start at an index using indexOf(String_searching_for, index to start at)
+//		String str1 = "Hello there yogi there";
+//		
+//		int b = str1.indexOf("there", 7);
+//		System.out.println(b);
+		
+		//problem 5 last index of
+		String str = "Hello there yogi there";
+		int a = str.lastIndexOf("yogi");
+		System.out.println(a);
+		
+//		String str = "Hello yogi there yogi there";
+//		
+//		int a = str.indexOf("yogi");
+//		int b = str.lastIndexOf("yogi");
+//		
+//		System.out.println("Index of yogi " + a );
+//		System.out.println("last index of yogi " + b );
+//		
+//			
+	}
 
-	String a = "hello";
-	String b = "there";
-	
-	a.charAt(3);
-	System.out.println(a.charAt(4));
 }
-
-}
-
-
