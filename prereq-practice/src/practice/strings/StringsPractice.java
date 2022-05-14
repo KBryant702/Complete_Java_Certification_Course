e14133551221b8a2c8fab9520a36b03716297165
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-13 21:32:52 -0700

    Strings

---
+++ prereq-practice/src/practice/strings/StringsPractice.java
@@ -0,0 +1,15 @@
+package practice.strings;
+
+public class StringsPractice {
+
+	public static void main(String[] args) {
+		String a = "hello";
+		
+		String str = "ABCDEFG";
+		String extractedString = str.substring(1);
+		
+		System.out.println(extractedString);
+
+	}
+
+}
