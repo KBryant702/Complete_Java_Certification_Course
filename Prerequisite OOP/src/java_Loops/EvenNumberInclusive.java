603e74ed08e93866e801ade241f112d2e8ef34b4
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-15 12:12:42 -0700

    Even numbers to 100 inclusive

---
+++ Prerequisite OOP/src/java_Loops/EvenNumberInclusive.java
@@ -0,0 +1,12 @@
+package java_Loops;
+
+public class EvenNumberInclusive {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 100; i+=2 ) { // i+= 2 current value of i add two and reassign back to itself
+		System.out.println(i);
+		}
+
+	}
+}	
--- Prerequisite OOP/src/java_Loops/ForLoop.java
+++ Prerequisite OOP/src/java_Loops/ForLoop.java
@@ -4,10 +4,10 @@ public class ForLoop {
 	
 	public static void main(String[] args) {
 		
-	String name = "fhjkahfjdkhsajkfhjksahdjk";
+		String name = "fhjkahfjdkhsajkfhjksahdjk";
 	
-	for (int i = name.length()-1; i>= 0; i--) {
-		System.out.println("char:: " + name.charAt(i));
-	}
+		for (int i = name.length()-1; i>= 0; i--) {
+			System.out.println("char:: " + name.charAt(i));
+		}
 	}
 }
