c06afcb11a5b5a2f74c6dff4e050758364812f9a
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-15 11:47:41 -0700

    restructure files

---
+++ Prerequisite OOP/src/java_Loops/Practice_Loops.java
@@ -0,0 +1,30 @@
+package java_Loops;
+
+public class Practice_Loops {
+
+	public static void main(String[] args) {
+		//first lesson
+		//structure of a while loop :    while (test_is_true) { do_this_body}
+		//while loop I tried on my own
+//		int count = 0;
+//		
+//		
+//		while(count <= 10) {
+//			System.out.println("Kim " + count);
+//			count++;
+//		}
+		//while loop example
+		int count = 0;
+		
+		
+		while(count <= 100) {
+			System.out.println("Kim is Looping " + count);
+			count = count +1;
+			
+			if(count == 30) {
+			break;
+			}
+		}
+	}
+
+}
