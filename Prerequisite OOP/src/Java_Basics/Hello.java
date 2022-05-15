772c4027d70b0677e8042fcb7a4df495c076775e
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-15 11:50:28 -0700

    updated file structure

---
+++ Prerequisite OOP/src/Java_Basics/Hello.java
@@ -0,0 +1,9 @@
+package Java_Basics;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        //To print something to the console/screen
+        System.out.println("Hello there, my name is Kim!");
+    }
+}
---
+++ Prerequisite OOP/src/Java_Basics/Variables.java
@@ -0,0 +1,33 @@
+package Java_Basics;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        //declaring integer variable x
+        int x;
+        // assigns value of 4 to x
+        x = 4;
+        System.out.println(x);
+        //reassign the variable
+        x = 43;
+        System.out.println(x);
+
+        //declare String variable type words
+        String words;
+        // assigned "This is a sentence" to words
+        words = "This is a sentence";
+        System.out.println(words);
+
+        //assign and declare value of x
+        int y = 22;
+        System.out.println(y);
+        //reassign the variable
+        y = 44;
+        System.out.println(y);
+
+        //declare and assign String variable type words
+        String words1 = "This is a sentence";
+        System.out.println(words1);
+    }
+
+}
