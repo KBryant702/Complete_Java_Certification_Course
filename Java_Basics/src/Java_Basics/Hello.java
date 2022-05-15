e74a2192c3e9bd779495ae7e82aa07f5fe40bbc3
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-15 11:46:42 -0700

    changes made

--- Java_Basics/src/Java_Basics/Hello.java
+++
@@ -1,9 +0,0 @@
-package Java_Basics;
-
-public class Hello {
-
-    public static void main(String[] args) {
-        //To print something to the console/screen
-        System.out.println("Hello there, my name is Kim!");
-    }
-}
