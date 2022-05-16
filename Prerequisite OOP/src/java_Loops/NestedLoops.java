defbcbe31e975c340c2b6e8b7acc3fe0920af38e
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-16 08:05:52 -0700

    Nested For Loops

--- Prerequisite OOP/src/java_Loops/NestedLoops.java
+++ Prerequisite OOP/src/java_Loops/NestedLoops.java
@@ -4,14 +4,15 @@ public class NestedLoops {
 
 	public static void main(String[] args) {
 		
-		for(int idx = 0; idx < 10; idx++) {
-			
-			for(int j = 0; j <10; j+=2) {
-				for(int i = 0; i <1; i++) {
-				System.out.println("the value of idx was " + idx + " value of j is " + j + " value of i is "+ i);
-				}
+		int counter = 0;
+		for(int idx = 0; idx < 100; idx++) {
+			//it is easier to think of loops from inside out. Look at the inner most loop first
+			for(int j = 0; j <10; j+=2) { // max two loops it slows down the program too much.
+				System.out.println("the value of idx was " + idx + " value of j is " + j);
+				counter++;
 			}
 		}
+		System.out.println("Counter: "+ counter);
 	}
 
 }
