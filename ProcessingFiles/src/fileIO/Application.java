e6e1bc1721064a7640e2a6ee5e0382eba93001dc
Kim <99856852+KBryant702@users.noreply.github.com>
2022-05-29 10:17:22 -0700

    File reader

--- ProcessingFiles/src/fileIO/Application.java
+++ ProcessingFiles/src/fileIO/Application.java
@@ -1,16 +1,27 @@
 package fileIO;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
-		for (int i = 0; i <= 3; i++) {
-			Scanner input = new Scanner(System.in);
-			System.out.println("Enter some Text");
-			String enteredText = input.nextLine();
-			System.out.println(enteredText);
-		}
+		//Scanner class
+//		for (int i = 0; i <= 3; i++) {
+//			Scanner input = new Scanner(System.in);
+//			System.out.println("Enter some Text");
+//			String enteredText = input.nextLine();
+//			System.out.println(enteredText);
+//		}
+		//File class
+				File file = new File("myFile.txt");
+				Scanner input = new Scanner(file);
+				while(input.hasNextLine()) {
+					String line = input.nextLine();
+					System.out.println(line);
+				}
+				input.close();
 	}
 }
