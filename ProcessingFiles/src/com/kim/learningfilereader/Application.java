20edfe9f2a773061862a7b77977534babc61ac7f
Kim <99856852+KBryant702@users.noreply.github.com>
2022-06-05 12:32:01 -0700

    Try with resources

--- ProcessingFiles/src/com/kim/learningfilereader/Application.java
+++ ProcessingFiles/src/com/kim/learningfilereader/Application.java
@@ -1,3 +1,4 @@
+
 package com.kim.learningfilereader;
 
 import java.io.*;
@@ -6,12 +7,12 @@ public class Application {
 
 	public static void main(String[] args) {
 	
-		File file = new File("myFile5.txt");
-		BufferedReader bufferedReader = null;
-		try {
-			FileReader fileReader = new FileReader(file);
-			//buffers characters into lines from the file reader object reading line by line
-			bufferedReader = new BufferedReader(fileReader);
+		File file = new File("myFile.txt");
+		//BufferedReader and FileReader no longer needed to be defined here now they are defined in the try with resources
+
+		//Try with resources automatically closes these resources
+		try (FileReader fileReader = new FileReader(file);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);){
 			
 			String line = bufferedReader.readLine();
 			
@@ -24,12 +25,7 @@ public class Application {
 			//catches a reading or writing error(Input OutPut errors
 		} catch (IOException e) {
 			System.out.println("Problem reading the file" + file.getName());
-		}finally {
-			try {
-				bufferedReader.close();
-			} catch (IOException e) {
-				System.out.println("Unable to close file" + file.getName());
-			}
+			
 		}
 	}
 
