3ad99969784ba4ff37e45e8428f7fdf45e310fa0
Kim <99856852+KBryant702@users.noreply.github.com>
2022-06-05 10:44:42 -0700

    Custom Exception with package

--- ProcessingFiles/src/fileIO/MyFileUtils.java
+++ ProcessingFiles/src/fileIO/MyFileUtils.java
@@ -1,17 +1,14 @@
 package fileIO;
 
+import com.kim.exceptions.FooRuntimeException;
+
 public class MyFileUtils {
  public int subtract10FromLargerNumber (int number) throws FooRuntimeException{
 	 if (number <10) {
 		 throw new FooRuntimeException("The number passed was smaller than 10");
 	 }
 	 return number -10;
- }
- public class FooRuntimeException extends Exception{
 
-	public FooRuntimeException(String message) {
-		super(message);
-	}
 	 
  }
 }
