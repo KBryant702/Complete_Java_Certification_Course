674fc787a4a991f4f26b398d8bd5980b6e24e956
Kim <99856852+KBryant702@users.noreply.github.com>
2022-06-07 20:01:22 -0700

    Type Safety

--- ProcessingFiles/src/com/kim/learningfilereader/Application2.java
+++ ProcessingFiles/src/com/kim/learningfilereader/Application2.java
@@ -1,10 +1,25 @@
 package com.kim.learningfilereader;
 
+class MyClass implements AutoCloseable{
+
+	@Override
+	public void close() throws Exception {
+		//should contain closing behaviors here
+		
+		
+		System.out.println("Closing!");
+	}
+	
+}
 public class Application2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		try(MyClass var = new MyClass();){
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
